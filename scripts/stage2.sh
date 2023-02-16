a03d285c5f3eb1fbc2b32fb432e0e9e098bcffab
Richard Wohlbold <richard@rgwohlbold.de>
2023-02-16 15:27:01 +0100

    Finish OpenWRT build

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-make "-j$(nproc)" package/network/services/gaffa/compile
+make "-j$(nproc)" V=sc package/network/services/gaffa/compile
 cp bin/targets/mvebu/cortexa9/packages/gaffa*.ipk out
