c6d2e7ddf54ab6166ae1bc233ef3a82f6321ea57
Aaron <16378455+dasGoogle@users.noreply.github.com>
2023-03-14 11:04:22 +0100

    Add GHA build script

---
+++ scripts/build-gha.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+mkdir -p out package/src/src
+cp scripts/Makefile package/
+cp CMakeLists.txt package/src/
+cp -r src package/src/
+
+docker run --rm \
+  -v "$(pwd)"/scripts/stage2.sh:/home/build/openwrt/stage2.sh \
+  -v "$(pwd)"/package/:/home/build/openwrt/package/network/services/gaffa \
+  -v "$(pwd)"/out/:/home/build/openwrt/out\
+  wimove-buildenv/arm \
+  bash stage2.sh
