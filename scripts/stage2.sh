05294216e4ea3ce24a8b1dc0680e11a664dbbcc2
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-21 12:08:17 +0100

    Wait

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,5 +2,9 @@
 set -ex
 shopt -s globstar
 cd /home/build/openwrt
+while [ ! -d "package/network/services/gaffa" ]; do
+  echo "waiting for package/network/services/gaffa"
+  sleep 1
+done
 make "-j$(nproc)" V=sc package/network/services/gaffa/compile
 cp bin/targets/**/packages/gaffa*.ipk out
