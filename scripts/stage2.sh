6bce962038b327c8823e2c97716093f7dba6fc62
Aaron Schlitt <aaron.schlitt@student.hpi.de>
2023-03-21 13:27:16 +0100

    Remove debug statement

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -ex
 shopt -s globstar
-ls -lisa /home/build/openwrt/package/network/services/
 cd /home/build/openwrt
 make "-j$(nproc)" V=sc prereq
 make "-j$(nproc)" V=sc package/network/services/gaffa/compile
