5d63bd2961db586bd92a01acf994a045fbf86a1f
Aaron Schlitt <aaron.schlitt@student.hpi.de>
2023-03-21 12:10:22 +0100

    Re-add ls command

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 shopt -s globstar
+ls -lisa /home/build/openwrt/package/network/services/
 cd /home/build/openwrt
 make "-j$(nproc)" V=sc package/network/services/gaffa/compile
 cp bin/targets/**/packages/gaffa*.ipk out
