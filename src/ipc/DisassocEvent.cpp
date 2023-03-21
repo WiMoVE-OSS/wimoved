aa3c8e605cf9992ac3b090605cce00ddc450404d
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-21 10:57:08 +0100

    Revert "Use only netlink for event ingestion (#3)" (#14)
    This reverts commit 1278176b220cdfeea970c7c09b2773979d6ce069.

---
+++ src/ipc/DisassocEvent.cpp
@@ -0,0 +1,7 @@
+#include "DisassocEvent.h"
+
+#include <utility>
+
+void ipc::DisassocEvent::handle(EventHandler *handler) { handler->handle_disassoc(this); }
+
+ipc::DisassocEvent::DisassocEvent(Station station) : Event(std::move(station)) {}
