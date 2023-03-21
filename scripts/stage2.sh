4936c5ceb937dea59a51ab6423d7ee2590a29a4a
Aaron Schlitt <aaron.schlitt@student.hpi.de>
2023-03-21 13:24:42 +0100

    Make multi-threaded again

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -3,5 +3,6 @@ set -ex
 shopt -s globstar
 ls -lisa /home/build/openwrt/package/network/services/
 cd /home/build/openwrt
-make V=sc package/network/services/gaffa/compile
+make "-j$(nproc)" V=sc prereq
+make "-j$(nproc)" V=sc package/network/services/gaffa/compile
 cp bin/targets/**/packages/gaffa*.ipk out
