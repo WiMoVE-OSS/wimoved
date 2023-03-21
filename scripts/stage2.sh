aabec3e8e550237426f349d621bc59c12fe47a90
Aaron Schlitt <aaron.schlitt@student.hpi.de>
2023-03-21 13:19:35 +0100

    Make single-threaded

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -3,5 +3,5 @@ set -ex
 shopt -s globstar
 ls -lisa /home/build/openwrt/package/network/services/
 cd /home/build/openwrt
-make "-j$(nproc)" V=sc package/network/services/gaffa/compile
+make V=sc package/network/services/gaffa/compile
 cp bin/targets/**/packages/gaffa*.ipk out
