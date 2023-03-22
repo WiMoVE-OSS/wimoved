0bc9fb1aa594c1d5f247f1b0a61214b5f67fcdcc
Aaron Schlitt <aaron.schlitt@student.hpi.de>
2023-03-22 13:50:51 +0100

    Add trailing slash to socket folder

--- src/ipc/Socket.cpp
+++ src/ipc/Socket.cpp
@@ -45,7 +45,7 @@ static std::string random_name() {
 
 ipc::Socket::Socket(const std::chrono::duration<int>& timeout, const std::string& sockname)
     : local{AF_UNIX, "\0"}, dest() {
-    std::string socket_path = Configuration::get_instance().hapd_sockdir + sockname;
+    std::string socket_path = Configuration::get_instance().hapd_sockdir + "/" + sockname;
     std::string local_path = "/var/run/wimoved." + random_name();
     if (unlink(local_path.c_str()) == 0) {
         WMLOG(DEBUG) << "Successfully unlinked socket" << local_path;
