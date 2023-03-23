e39f8968e09c386017dfa131feceb7c4fd5ee635
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-23 14:54:29 +0100

    Fix immediate crash

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -11,12 +11,12 @@
 
 // trim from start (in place)
 static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) { return std::isspace(ch) != 0; }));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) { return std::isspace(ch) == 0; }));
 }
 
 // trim from end (in place)
 static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return std::isspace(ch) != 0; }).base(), s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return std::isspace(ch) == 0; }).base(), s.end());
 }
 
 // trim from both ends (in place)
