efe695812973463ef9479a5637f1361fca9fc3ac
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-28 15:32:33 +0200

    Remove unnecessary header files

--- src/nl/LinkCache.cpp
+++ src/nl/LinkCache.cpp
@@ -1,12 +1,10 @@
 #include "LinkCache.h"
 
-#include <netlink/cache.h>
-#include <netlink/netlink.h>
-#include <netlink/route/link/vxlan.h>
+#include <netlink/route/link.h>
 
 #include <stdexcept>
 
-LinkCache::LinkCache(struct nl_sock *socket) : cache(nullptr) {
+LinkCache::LinkCache(struct nl_sock *socket) {
     int err = rtnl_link_alloc_cache(socket, AF_INET, &cache);
     if (err < 0) {
         throw std::runtime_error(std::string("Could not allocate cache: ") + nl_geterror(err));
