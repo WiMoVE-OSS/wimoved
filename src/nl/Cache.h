bbe3bdad9b09633388cb6e0354d02413136f63fe
Richard Wohlbold <richard@rgwohlbold.de>
2023-03-28 19:01:23 +0200

    Delete move constructors in nl::Cache

--- src/nl/Cache.h
+++ src/nl/Cache.h
@@ -1,14 +1,18 @@
 #ifndef WIMOVED_CACHE_H
 #define WIMOVED_CACHE_H
 
+#include <netlink/cache.h>
+
 class Cache {
    public:
     explicit Cache(struct nl_cache* cache);
     ~Cache();
     Cache(const Cache& other) = delete;
     Cache operator=(const Cache& other) = delete;
+    Cache(Cache&& other) = delete;
+    Cache operator=(Cache&& other) = delete;
 
-    struct nl_cache* cache = nullptr;
+    nl_cache* cache;
 };
 
 #endif  // WIMOVED_CACHE_H
