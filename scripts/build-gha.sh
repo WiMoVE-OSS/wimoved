95c597a695d032d410815c6946547142dbf14091
Alexander Sohn <github@asohn.de>
2023-03-30 15:58:28 +0200

    Just use volumes.

--- scripts/build-gha.sh
+++ scripts/build-gha.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+mkdir -p out || true
+chmod ogu+rw out
 docker run --rm \
-  -v "$(pwd)"scripts/openwrtfiles:/home/build/openwrt/package/network/services/wimoved \
-  -v "$(pwd)"CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src \
-  -v "$(pwd)"vendor:/home/build/openwrt/package/network/services/wimoved/src \
-  -v "$(pwd)"src:/home/build/openwrt/package/network/services/wimoved/src \
+  -v "$(pwd)"/scripts/openwrtfiles:/home/build/openwrt/package/network/services/wimoved \
+  -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
+  -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
+  -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
   -v "$(pwd)"/out:/home/build/openwrt/out \
-  $IMAGE
+  buildenv
+
