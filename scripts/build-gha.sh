8d26da38207fbe28eadd9d680978b327d69f671e
Alexander Sohn <github@asohn.de>
2023-03-30 14:44:59 +0200

    Try to build without src/src

--- scripts/build-gha.sh
+++ scripts/build-gha.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -xe
-rm -r out package || true
-mkdir -p out package/src/src
 chmod -R ogu+rw out
-cp scripts/openwrtfiles/* package/
-cp CMakeLists.txt package/
-cp -r src package/
-cp -r vendor package/
 
 docker run --rm \
-  -v "$(pwd)"/package:/home/build/openwrt/package/network/services/wimoved \
+  -v "$(pwd)"scripts/openwrtfiles:/home/build/openwrt/package/network/services/wimoved \
+  -v "$(pwd)"CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src \
+  -v "$(pwd)"vendor:/home/build/openwrt/package/network/services/wimoved/src \
+  -v "$(pwd)"src:/home/build/openwrt/package/network/services/wimoved/src \
   -v "$(pwd)"/out:/home/build/openwrt/out \
   $IMAGE
