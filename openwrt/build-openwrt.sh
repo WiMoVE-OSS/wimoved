d466b363b28a547bcfcd1b4321a6c64f1f1208b8
Alexander Sohn <github@asohn.de>
2023-03-30 18:50:13 +0200

    Make directories single word and scripts kebab case.

--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -2,7 +2,7 @@
 mkdir -p out || true
 chmod -R ogu+rw out
 docker run --rm \
-  -v "$(pwd)"/OpenWrtBuild/packageDirectory:/home/build/openwrt/package/network/services/wimoved \
+  -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
   -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
   -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
   -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
