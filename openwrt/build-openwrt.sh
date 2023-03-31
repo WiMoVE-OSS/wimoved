d84956febc90f5b5ff26b76fb0af3b01d351b725
Alexander Sohn <39831045+Sohn123@users.noreply.github.com>
2023-03-31 10:36:59 +0200

    Tidy up scripts directory (#54)
    * Ignore ninja build files.
    
    * First restructuring
    
    * Change the variable back
    
    * Try to build without src/src
    
    * Try to build without src/src
    
    * Don't do anything with the directories.
    
    * Just use volumes.
    
    * Use proper image.
    
    * Include wimoved file
    
    * Change permissions recursively.
    
    * Remove old script.
    
    * Rename directories.
    
    * Fix volumes.
    
    * Shrink docker volume.
    
    * Make directories single word and scripts kebab case.
    
    * Remove package form .gitignore
    
    * Fix cmd in buildenv container.

---
+++ openwrt/build-openwrt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p out || true
+chmod -R ogu+rw out
+docker run --rm \
+  -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
+  -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
+  -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
+  -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
+  -v "$(pwd)"/out:/home/build/openwrt/out \
+  $IMAGE
