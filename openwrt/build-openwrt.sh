b1f919fce2cd96b966e63a17bfc7ef25d032fb47
Alexander Sohn <github@asohn.de>
2023-04-11 16:38:29 +0200

    Fix env variable in build script.

--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -2,7 +2,7 @@
 mkdir -p out || true
 chmod -R ogu+rw out
 docker run --rm \
-  -e PKG_VERSION="$(VERSION)+beta$(date '+%Y-%m-%d-%H.%M.%S')" \
+  -e PKG_VERSION="$($VERSION)+beta$(date '+%Y-%m-%d-%H.%M.%S')" \
   -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
   -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
   -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
