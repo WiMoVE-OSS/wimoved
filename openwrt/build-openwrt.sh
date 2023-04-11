62d8976c6e8b8a984a80548be4d46f6773ad0439
Alexander Sohn <github@asohn.de>
2023-04-11 16:12:22 +0200

    Change pipelines to contain versions.

--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -2,9 +2,10 @@
 mkdir -p out || true
 chmod -R ogu+rw out
 docker run --rm \
+  -e PKG_VERSION="$(VERSION)+beta$(date '+%Y-%m-%d-%H.%M.%S')" \
   -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
   -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
   -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
   -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
   -v "$(pwd)"/out:/home/build/openwrt/out \
-  $IMAGE
+  "$($IMAGE)"
