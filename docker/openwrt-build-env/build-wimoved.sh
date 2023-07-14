93a1c6a7882a8a3dfa1a5dbf46d37770850701a5
Richard Wohlbold <richard@rgwohlbold.de>
2023-07-14 13:40:00 +0200

    Use new openwrt image (#64)
    * Fix build
    
    * Rename my_packages to wimove_feed
    
    * Reduce number of layers, add V=s
    
    * Remove comments from prometheus makefile
    
    * Remove comment
    
    * Remove one chown, fix out/ mountpoint
    
    ---------
    
    Co-authored-by: Alexander Sohn <github@asohn.de>

--- docker/openwrt-build-env/build-wimoved.sh
+++ docker/openwrt-build-env/build-wimoved.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 shopt -s globstar
-cd /home/build/openwrt
-make "-j$(nproc)" V=sc prereq
-make "-j$(nproc)" V=sc package/network/services/wimoved/compile
+./scripts/feeds update wimove_feed
+./scripts/feeds install wimoved
+make "-j$(nproc)" V=sc package/wimoved/compile
 cp bin/targets/**/packages/wimoved*.ipk out
--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir -p out || true
 chmod -R ogu+rw out
-docker run --rm \
+docker run --rm --ulimit nofile=1024:524288 \
   -e PKG_VERSION="$VERSION" \
-  -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
-  -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
-  -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
-  -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
-  -v "$(pwd)"/out:/home/build/openwrt/out \
+  -v "$(pwd)"/openwrt/package:/builder/wimove_feed/network/services/wimoved \
+  -v "$(pwd)"/CMakeLists.txt:/builder/wimove_feed/network/services/wimoved/src/CMakeLists.txt \
+  -v "$(pwd)"/vendor:/builder/wimove_feed/network/services/wimoved/src/vendor \
+  -v "$(pwd)"/src:/builder/wimove_feed/network/services/wimoved/src/src \
+  -v "$(pwd)"/out:/builder/out \
   "$IMAGE"
