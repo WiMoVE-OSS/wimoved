59acfc77ca25425601435d1f13a0ad6c9bae17f9
Richard Wohlbold <richard@rgwohlbold.de>
2023-07-14 12:02:32 +0200

    Rename my_packages to wimove_feed

--- docker/openwrt-build-env/build-wimoved.sh
+++ docker/openwrt-build-env/build-wimoved.sh
@@ -2,7 +2,7 @@
 set -ex
 shopt -s globstar
 #make "-j$(nproc)" V=sc prereq
-./scripts/feeds update my_packages
+./scripts/feeds update wimove_feed
 ./scripts/feeds install wimoved
 make "-j$(nproc)" V=sc package/wimoved/compile
 cp bin/targets/**/packages/wimoved*.ipk out
--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -3,9 +3,9 @@ mkdir -p out || true
 chmod -R ogu+rw out
 docker run --rm --ulimit nofile=1024:524288 \
   -e PKG_VERSION="$VERSION" \
-  -v "$(pwd)"/openwrt/package:/builder/my_packages/network/services/wimoved \
-  -v "$(pwd)"/CMakeLists.txt:/builder/my_packages/network/services/wimoved/src/CMakeLists.txt \
-  -v "$(pwd)"/vendor:/builder/my_packages/network/services/wimoved/src/vendor \
-  -v "$(pwd)"/src:/builder/my_packages/network/services/wimoved/src/src \
+  -v "$(pwd)"/openwrt/package:/builder/wimove_feed/network/services/wimoved \
+  -v "$(pwd)"/CMakeLists.txt:/builder/wimove_feed/network/services/wimoved/src/CMakeLists.txt \
+  -v "$(pwd)"/vendor:/builder/wimove_feed/network/services/wimoved/src/vendor \
+  -v "$(pwd)"/src:/builder/wimove_feed/network/services/wimoved/src/src \
   -v "$(pwd)"/out:/out \
   "$IMAGE"
