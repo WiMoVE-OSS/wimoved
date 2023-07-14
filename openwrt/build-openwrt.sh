8001674196720b9ff801be36019e054426ecf2c4
Alexander Sohn <github@asohn.de>
2023-07-14 11:07:06 +0200

    Make work public

--- openwrt/build-openwrt.sh
+++ openwrt/build-openwrt.sh
@@ -3,9 +3,9 @@ mkdir -p out || true
 chmod -R ogu+rw out
 docker run --rm \
   -e PKG_VERSION="$VERSION" \
-  -v "$(pwd)"/openwrt/package:/home/build/openwrt/package/network/services/wimoved \
-  -v "$(pwd)"/CMakeLists.txt:/home/build/openwrt/package/network/services/wimoved/src/CMakeLists.txt \
-  -v "$(pwd)"/vendor:/home/build/openwrt/package/network/services/wimoved/src/vendor \
-  -v "$(pwd)"/src:/home/build/openwrt/package/network/services/wimoved/src/src \
-  -v "$(pwd)"/out:/home/build/openwrt/out \
+  -v "$(pwd)"/openwrt/package:/builder/package/network/services/wimoved \
+  -v "$(pwd)"/CMakeLists.txt:/builder/package/network/services/wimoved/src/CMakeLists.txt \
+  -v "$(pwd)"/vendor:/builder/package/network/services/wimoved/src/vendor \
+  -v "$(pwd)"/src:/builder/package/network/services/wimoved/src/src \
+  -v "$(pwd)"/out:/out \
   "$IMAGE"
